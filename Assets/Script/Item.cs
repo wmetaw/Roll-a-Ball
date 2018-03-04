2f9808d9c63356bc46cc9e06b34bd1ab3e26a2d6
meta <lio114514@gmail.com>
2018-03-04 13:06:29 +0900

    items

---
+++ Assets/Script/Item.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+	//	OnTriggerEnter   ： 判定のみのTriggerとの接触判定
+	//	OnCollisionEnter ： 物理的な反射を持つColliderとの接触判定
+
+	// Trigger接触時に呼ばれるコールバック
+	void OnTriggerEnter(Collider hit) {
+		if(hit.CompareTag("Player")) {
+			Destroy (gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
