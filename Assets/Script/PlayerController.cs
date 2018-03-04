6157e07b1846ea4dd97a2d7b243d5bb1db40f06b
meta <lio114514@gmail.com>
2018-03-04 12:40:39 +0900

    player

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 10;
+
+	void FixedUpdate() {
+		// 入力をxとzに代入
+		float x = Input.GetAxis("Horizontal"); // 水平
+		float z = Input.GetAxis("Vertical");   // 垂直
+
+		// 同一のgameobjectが持つRididbodyコンポーネントを取得
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+		rigidbody.AddForce (x * speed, 0, z * speed);
+	}
+}
