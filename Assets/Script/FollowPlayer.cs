eea108d070b74fc80e9b434bc55052d66cb43dcc
meta <lio114514@gmail.com>
2018-03-04 12:49:34 +0900

    move camera

---
+++ Assets/Script/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Transform target; // targetへの参照
+	private Vector3 offset;  // 相対座標
+
+	void Start() {
+		offset = GetComponent<Transform> ().position - target.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// 自分の座標にtarget属性を代入する
+		GetComponent<Transform>().position = target.position + offset;
+	}
+}
