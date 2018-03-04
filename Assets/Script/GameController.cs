aaf335021203249c18f567e1b8c22721b5783ec4
meta <lio114514@gmail.com>
2018-03-04 13:37:25 +0900

    game clear

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 
 	public UnityEngine.UI.Text scoreLabel;
+	public GameObject winnerLabelObject;
 
 	// Update is called once per frame
 	void Update () {
 		int count = GameObject.FindGameObjectsWithTag ("Item").Length;
 		scoreLabel.text = count.ToString ();
+
+		if (count == 0) {
+			winnerLabelObject.SetActive (true);
+		}
 	}
 }
