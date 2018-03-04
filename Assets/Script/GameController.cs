561a97c4a064192f6c775d2b79b3e218c02e8214
meta <lio114514@gmail.com>
2018-03-04 13:30:09 +0900

    UI

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public UnityEngine.UI.Text scoreLabel;
+
+	// Update is called once per frame
+	void Update () {
+		int count = GameObject.FindGameObjectsWithTag ("Item").Length;
+		scoreLabel.text = count.ToString ();
+	}
+}
