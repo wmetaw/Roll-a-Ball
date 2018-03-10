c0cff1d533de36c1c12aa2daf396dece7c230913
meta <lio114514@gmail.com>
2018-03-10 23:59:59 +0900

    obstacle and restart

---
+++ Assets/DangerWall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class DangerWall : MonoBehaviour {
+
+
+	// オブジェクト接触した時に呼ばれるコールバック
+	void OnCollisionEnter(Collision hit)
+	{
+		// 接触したオブジェクトのタグがplayerの場合
+		if (hit.gameObject.CompareTag ("Player")) {
+			
+			// 現在のシーン番号を取得
+			int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+			// 現在のシーンを再読み込み
+			SceneManager.LoadScene(sceneIndex);
+		}
+	}
+}
